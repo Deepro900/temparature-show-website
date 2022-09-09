c9289aa0430cf510afa2a1859963dd8f2056afff
Deepro900 <deeprobhattacharjee@gmail.com>
2022-09-09 22:10:35 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const api_key = `fa4845a4849be3385dd5f360b9333fef`;
+
+const loadTemperature = city => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const displayTemperature = data => {
+    const temperature = document.getElementById('temperature');
+    //temperature.innerText=
+    //console.log(data.main.temp);
+    //  temperature.innerText = data.main.temp;
+    setInnerText('temperature', data.main.temp);
+    setInnerText('condition', data.weather[0].main)
+    console.log(data.weather[0].main);
+}
+
+const setInnerText = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function() {
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+    loadTemperature(city);
+
+})
+
+loadTemperature('dhaka');
